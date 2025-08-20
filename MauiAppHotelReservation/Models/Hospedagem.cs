6f20a32e608eb173d22a24ff84ab9e2f4775f1ce
Davi Pedrosa <davipedrosassss@gmail.com>
2025-08-19 21:02:00 -0300

    Criando Modelos de negocios e atualizando o backend

---
+++ MauiAppHotelReservation/Models/Hospedagem.cs
@@ -0,0 +1,24 @@
+﻿namespace MauiAppHotelReservation.Models
+{
+    public class Hospedagem
+    {
+        public Quarto QuartoSelecionado { get; set; }
+        public int QuantidadeAdultos { get; set; }
+        public int QuantidadeCriancas { get; set; }
+        public DateTime DataCheckIn { get; set; }
+        public DateTime DataCheckOut { get; set; }
+        public int Estadia 
+        {
+            get => DataCheckOut.Subtract(DataCheckIn).Days;
+        }
+        public double ValorTotal
+        {
+            get
+            {
+                double valorAdultos = QuartoSelecionado.ValorDiariaAdulto * QuantidadeAdultos * Estadia;
+                double valorCriancas = QuartoSelecionado.ValorDiariaCriança * QuantidadeCriancas * Estadia;
+                return valorAdultos + valorCriancas;
+            }
+        }
+    }
+}
--- MauiAppHotelReservation/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotelReservation/Views/ContratacaoHospedagem.xaml.cs
@@ -1,8 +1,12 @@
+using MauiAppHotelReservation.Models;
+
 namespace MauiAppHotelReservation.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
 {
-	App PropriedadesDoApp;
+    // Declaração de uma variável para acessar as propriedades do App
+    App PropriedadesDoApp;
+
     public ContratacaoHospedagem()
     {
         InitializeComponent();
@@ -25,21 +29,47 @@ public partial class ContratacaoHospedagem : ContentPage
         // Define a data máxima de check-out para até 6 meses após a data de check-in
         dtpck_check_out.MaximumDate = dtpck_check_in.Date.AddMonths(6);
     }
-    private void btn_avancar_Clicked(System.Object sender, System.EventArgs e)
+
+
+    private async void btn_avancar_Clicked(System.Object sender, System.EventArgs e)
     {
 		try
 		{
-			//
-			Navigation.PushAsync(new Views.HospedagemContratada());
+            Hospedagem hospedagem = new Hospedagem 
+            {
+                // Obtém o quarto selecionado no Picker e converte para o tipo Quarto
+                QuartoSelecionado = (Quarto)pck_quarto.SelectedItem,
+
+                // Obtém a quantidade de adultos no stepper e converte para inteiro (ContrataçãoDeHospedagem.xaml Linha 54)
+                QuantidadeAdultos = Convert.ToInt32(stp_adultos.Value),
+
+                // Obtém a quantidade de crianças no stepper e converte para inteiro (ContrataçãoDeHospedagem.xaml Linha 67)
+                QuantidadeCriancas = Convert.ToInt32(stp_criancas.Value),
+
+                // Obtém a data de check-in selecionada no DatePicker
+                DataCheckIn = dtpck_check_in.Date,
+
+                // Obtém a data de check-out selecionada no DatePicker
+                DataCheckOut = dtpck_check_out.Date
+            };
+
+            // Cria uma nova instância do modelo Hospedagem e preenche com os dados do formulário
+           await Navigation.PushAsync(new Views.HospedagemContratada() 
+           {
+               // Passa a hospedagem criada para a próxima página (HospedagemContratada.xaml)
+               BindingContext = hospedagem
+           });
         
 		} 
 		catch (Exception ex) 
 		{
             // Exibe uma mensagem de erro caso ocorra uma exceção
-            DisplayAlert("Erro", ex.Message, "OK");
+            await DisplayAlert("Erro", ex.Message, "OK");
         
 		}
     }
+
+
 
     // Evento disparado quando a data de check-in é selecionada (ContrataçãoDeHospedagem.xaml Linha 100)
     private void dtpck_check_in_DateSelected(System.Object sender, Microsoft.Maui.Controls.DateChangedEventArgs e)
